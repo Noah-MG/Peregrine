badb11ed0437c49608bc4b08e1b124aa719f3196
Noah-MG <noah.glimcher28@avenues.org>
2026-03-09 12:52:22 -0400

    PeregrineOpMode class written

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/peregrine/core/PeregrineOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/peregrine/core/PeregrineOpMode.java
@@ -1,5 +1,10 @@
 package org.firstinspires.ftc.teamcode.peregrine.core;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.peregrine.editables.GlobalVariables;
+import org.firstinspires.ftc.teamcode.peregrine.editables.Hardware;
+
 /**
  * <h3>PeregrineOpMode is the basic class that all opModes extend from, replacing LinearOpMode in
  * the normal FTC SDK.</h3>
@@ -11,8 +16,51 @@ package org.firstinspires.ftc.teamcode.peregrine.core;
  * GlobalVariables class.</p>
  */
 
-public abstract class PeregrineOpMode {
+public abstract class PeregrineOpMode extends LinearOpMode {
 
+    /**This is the hardware object that contains all maps to robot hardware and that can be accessed through the opMode by any other class*/
+    public Hardware hardware;
 
+    /**This is the localizer object that keeps track of the robot's position using various sensors. It is specialized task that all opModes have.*/
+    public Localizer localizer;
+
+    /**This is the global variables object that contains every variable you use for your specific robot. It is in the editables package to allow you to add more detail.*/
+    public GlobalVariables globalVariables;
+
+    //This is the regular opMode function, being mapped to those below
+    public void runOpMode() {
+
+        initStart();
+
+        while(opModeInInit()) {
+            initLoop();
+        }
+
+        waitForStart();
+
+        mainStart();
+
+        while(opModeIsActive()) {
+            mainLoop();
+        }
+
+        end();
+
+    }
+
+    /**This gets run once at the start of init.*/
+    public abstract void initStart();
+
+    /**This gets run repeatedly throughout init until you press run.*/
+    public abstract void initLoop();
+
+    /**This gets run once immediately after you press start.*/
+    public abstract void mainStart();
+
+    /**This gets run repeatedly after the start button is pressed, it is where the main body of code is run.*/
+    public abstract void mainLoop();
+
+    /**This gets run once at the end of the opMode.*/
+    public abstract void end();
 
 }

65cdd8453472acfe4c946577c63343aa17f33dbc
Noah-MG <noah.glimcher28@avenues.org>
2026-03-09 12:30:49 -0400

    All major classes now added and planned

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/peregrine/core/PeregrineOpMode.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.peregrine.core;
+
+/**
+ * <h3>PeregrineOpMode is the basic class that all opModes extend from, replacing LinearOpMode in
+ * the normal FTC SDK.</h3>
+ *
+ * <p>PeregrineOpMode should never directly be mentioned by any of your code. It is further extended
+ * by PeregrineAutonomous and PeregrineTeleop, which should be used for their respective opMode
+ * types instead of PeregrineOpMode. It provides a universal interface that can be used by tasks
+ * to access to robot's hardware and any other global variables that you may choose to add to the
+ * GlobalVariables class.</p>
+ */
+
+public abstract class PeregrineOpMode {
+
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/peregrine/core/Task.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.peregrine.core;
+
+public class Task {
+}

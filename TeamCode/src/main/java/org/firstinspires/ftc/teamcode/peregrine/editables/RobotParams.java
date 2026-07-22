26f44419a1a03182373f25e61591590a9d8dd791
TriangleBoy <89360730+TriangleBoy@users.noreply.github.com>
2026-07-22 10:23:04 -0400

    Added Pinpoint and Motors to Hardware and RobotParams

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/peregrine/editables/RobotParams.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/peregrine/editables/RobotParams.java
@@ -1,10 +1,27 @@
 package org.firstinspires.ftc.teamcode.peregrine.editables;
 
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.peregrine.core.utilities.Enums;
 
 public final class RobotParams {
 
     public static Enums.Chassis chassis = Enums.Chassis.MECANUM;
 
+    public static DistanceUnit distanceUnit = DistanceUnit.CM;
+
+    public static double odoXOffset = 0;
+    public static double odoYOffset = 0;
+    public static GoBildaPinpointDriver.GoBildaOdometryPods podType = GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD;
+    public static GoBildaPinpointDriver.EncoderDirection xEncoderDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD;
+    public static GoBildaPinpointDriver.EncoderDirection yEncoderDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD;
+
+
+    public static DcMotor.Direction FRDirection = DcMotor.Direction.FORWARD;
+    public static DcMotor.Direction FLDirection = DcMotor.Direction.FORWARD;
+    public static DcMotor.Direction BRDirection = DcMotor.Direction.FORWARD;
+    public static DcMotor.Direction BLDirection = DcMotor.Direction.FORWARD;
 
 }
